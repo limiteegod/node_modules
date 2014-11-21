7a2ef1a9e99cb01e305db611441897f7395fedc3
LiMing <liming@localhost.localdomain>
2014-11-21 13:07:12 +0800

    log

--- easy_util/DigestUtil.js
+++ easy_util/DigestUtil.js
@@ -66,9 +66,9 @@ DigestUtil.prototype.generate = function(headNode, key, bodyStr)
     else if(headNode.digestType == "md5")
     {
         var text = bodyStr + headNode.timestamp + key;
-        log.info(text);
+        console.log(text);
         var md5 = self.md5(text);
-        log.info(md5);
+        console.log(md5);
         headNode.digest = md5;
         return bodyStr;
     }
