95157001c191c2aab47ffb76b6a7195901cb6231
shiqm <shiqingmao@ezcp.cn>
2014-12-23 10:50:57 +0800

    增加时间计算

--- mcp_util/DateMathUtil.js
+++ mcp_util/DateMathUtil.js
@@ -207,6 +207,29 @@ DateUtil.prototype.stringToDate = function stringToDate(DateStr) {
     return myDate;
 }
 
+//+---------------------------------------------------
+//| 求时间距现在多长时间
+//+---------------------------------------------------
+DateUtil.prototype.secondBetween = function secondBetween(DateTwo)
+{
+    var  str=DateTwo.toString();
+    str = str.replace(/-/g,"/");
+    var date2 = new Date(str);
+    var date3=new Date()-date2.getTime();  //时间差的毫秒数
+    //计算出相差天数
+    var days=Math.floor(date3/(24*3600*1000));
+    //计算出小时数
+    var leave1=date3%(24*3600*1000);    //计算天数后剩余的毫秒数
+    var hours=Math.floor(leave1/(3600*1000));
+    //计算相差分钟数
+    var leave2=leave1%(3600*1000);        //计算小时数后剩余的毫秒数
+    var minutes=Math.floor(leave2/(60*1000));
+    //计算相差秒数
+    var leave3=leave2%(60*1000);      //计算分钟数后剩余的毫秒数
+    var seconds=Math.round(leave3/1000);
+    return  days+"天 "+hours+"小时 "+minutes+"分钟"+seconds+"秒";
+}
+
 
 //+---------------------------------------------------
 //| 获取一天00:00:00
